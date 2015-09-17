5578b66d9b91f15d398a44b1c9b2822f924a0797
Joel McCarthy <joelmmccarthy@gmail.com>
2015-09-17 11:51:37 -0400

    clean-up 3

--- java-compile-run.sh
+++
@@ -1,6 +0,0 @@
-# Assuming in directory ~/Dropbox/Research/Philosophy of Science/Thinking with Models/Thinking-with-Models
-
-# Compiles the file, with classpath and destination folder.
-javac -cp .:/home/joel/anaconda/share/py4j/py4j0.8.2.1.jar:/opt/NetLogo/netlogo-5.0.5/NetLogo.jar -d bin/ src/py2NetLogo/NetLogoBridge.java
-# Runs the output class file.
-java -cp .:/home/joel/anaconda/share/py4j/py4j0.8.2.1.jar:/opt/NetLogo/netlogo-5.0.5/NetLogo.jar:./bin py2NetLogo.NetLogoBridge
